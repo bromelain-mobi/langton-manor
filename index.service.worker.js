b04adb455c77f125a16de17b8a5cd1eb4fbea92f
Bromelain Games <vladimir@bromelain.mobi>
2026-09-26 18:41:32 +0300

    1790436971

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1789973520|11813856";
+const CACHE_VERSION = "1790436971|2021915";
 const CACHE_PREFIX = "The Secret of La-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
